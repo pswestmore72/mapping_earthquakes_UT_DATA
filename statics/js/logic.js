58370269af38f7a20e732ab178e57f90ba59c5f7
unknown <pswestmore72@gmail.com>
2021-11-16 14:19:32 -0600

    pushing stuff

--- statics/js/logic.js
+++ statics/js/logic.js
@@ -45,3 +45,16 @@ cityData.forEach(function(city) {
     .addTo(map);
 });
 
+// Coordinates for each point to be used in the polyline.
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
+
+// Create a polyline using the line coordinates and make the line black.
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);
+
